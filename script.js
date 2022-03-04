e54c26185ef7cdaacb420aec25633fb05aec5d9e
Fabiany Lopes <53617138+fabianylopes@users.noreply.github.com>
2022-03-04 19:52:36 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function getAddress() {
+    const home = document.querySelector('.home-page');
+    home.classList.add('hidden');
+    
+    const address = document.querySelector('.get-address');
+    address.classList.remove('hidden');
+}
